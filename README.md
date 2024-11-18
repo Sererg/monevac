45917097f252c0ed975897af78084a3531bc001d
Sergey <svovyk@mail.ru>
2024-11-18 22:30:15 +0300

    last commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Пошаговое Руководство
+Клонирование Репозитория:
+
+git clone https://github.com/Sererg/monevac.git
+
+Переход в Директорию Проекта:
+
+cd monevac
+
+Установка Зависимостей:
+
+npm install
+
+Запуск Проекта:
+
+npm start
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sass|scss)$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader',
+            {
+                loader: 'sass-loader',
+                options: {
+                  sassOptions: {
+                    quietDeps: true, //отключает новые требования 
+                  },
+                },
+              },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    open: true,
+    port: 8080,
+    hot: true,
+  },
+  mode: 'development',
+};
